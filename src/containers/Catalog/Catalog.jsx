9f62cc0776abfd4a3fcc78f401912ce464736236
lucasbpro <lucasbpro@gmail.com>
2020-06-10 12:03:04 -0300

    Updated Catalog component to show items count

--- src/containers/Catalog/Catalog.jsx
+++ src/containers/Catalog/Catalog.jsx
@@ -8,19 +8,28 @@ import './Catalog.scss';
 
 const Catalog = ({opacityOn}) => {
 
+	const styleItemsCount = {
+		fontWeight: "bold",
+		margin: "10px 0 0 0" 
+		};
+
 	const products = useSelector(store => store.productList);
 
 	return(
 		<div style={ opacityOn ? { opacity: 0.4 } : null} 
-			 className="product-catalog" data-testid="product-catalog">
+			className="product-catalog-container">	
 
-			{products && products.map( (product, index)  => {
-				return ( 
-					<Link to={`/product/${product.style}`} key={index}> 
-						<ProductCard key={index} productInfo={product}/>
-					</Link>
-				)
-			})}
+			<p style={styleItemsCount}>{`(${products.length} itens)`}</p>
+
+			<div className="product-catalog" data-testid="product-catalog">
+				{products && products.map( (product, index)  => {
+					return ( 
+						<Link to={`/product/${product.style}`} key={index}> 
+							<ProductCard key={index} productInfo={product}/>
+						</Link>
+					)
+				})}
+			</div>
 		</div>
 	);
 };
