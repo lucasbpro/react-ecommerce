eaa934987b3ebcab997f97f39b0d24e807a32639
lucasbpro <lucasbpro@gmail.com>
2020-08-29 10:35:29 -0300

    fix: added JSON to replace request to API

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 
-//import Loading from '../../components/Loading';
+/* commented this because code is not using API 
+   instead, it is using mock data stored in the frontend app
+ import {URL_API} from "../../constants"; */
 
-import {URL_API} from "../../constants";
 import Catalog from "../../containers/Catalog";
 import {updateProductList} from '../../actions';
 import './Home.scss';
@@ -17,11 +18,17 @@ const Home = () => {
 
   // pulls data from API and updates global state
   useEffect(()=>{
+         /* commented this because code is not using API. Instead, it is using mock data 
+            stored in the frontend app.
         fetch(URL_API).then(response => response.json()).then((json)=>{
             if (json.length!==0 && json!=="No subscription") 
               setProductList(json)
-        }).then(dispatch(updateProductList(productList)));
-        },[dispatch,productList]);
+        }).then(dispatch(updateProductList(productList)));  */
+
+      const data = require('../../data/products.json');
+      setProductList(data.products);
+      dispatch(updateProductList(productList));
+  },[dispatch,productList]);
 
   useEffect(() => setIsLoading(false),[productList]);
 
